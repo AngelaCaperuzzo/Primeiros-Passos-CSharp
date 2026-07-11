58397b9f5469db9a99c047092783d741dff90da4
AngelaCaperuzzo <caperuzzo.angela@gmail.com>
2026-07-11 16:33:30 -0300

    feat: meu primeiro projeto em C#

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Aula1
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 29;
+            double saldo = 2.000;
+            char genero = 'F';
+            string nome = "Angela";
+
+            //Placeholder
+            Console.WriteLine(" Sr.a {0} do gênero {1}, com a seguinte idade {2}, possui em seu saldo o valor de {3:F3} yenes", nome, genero, idade, saldo); //saldo.ToString("F3")
+
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos, é do gênero {genero} e possui {saldo:F3} yenes.");
+
+            //Concatenação
+            Console.WriteLine(nome + " de " + idade + " anos, é do gênero " + genero + ", e ela possui o salde de " + saldo.ToString("F3", CultureInfo.InvariantCulture) + " yenes.");
+
+        }
+    }
+}
